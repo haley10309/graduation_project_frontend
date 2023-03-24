2e40efaca5a1fbdb06a927e528d971ef3b087ab1
hayung <yoohayung10309@gmail.com>
2023-03-24 10:37:18 +0900

    router set

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
 import Login from "./Login"
+import React, { Component } from 'react';
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+
+class cApp extends Component{
+  
+  state ={
+    proteins: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({proteins: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/proteinInput');
+    const body = await response.json();
+    return body;
+  }
+}
 
 function App() {
   return (
-    <div >
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <div >
+        <Routes>
+          
+          <Route path="/" element={ <Login/> } />
+            
+          
+        </Routes>
+      </div>
+    </BrowserRouter>
   );  
 }
 
