d23f15aca722d871dd9aca7622eabd374cf383b3
hayung <yoohayung10309@gmail.com>
2023-04-06 16:18:22 +0900

    Navbar reference edit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import Login from "./Login"
+import Login from "./pages/Home"
 import React, { Component } from 'react';
 import { BrowserRouter,Route, Routes } from "react-router-dom";
 import AFoutput from "./AFoutput";
+import NavBar from "./NavBar";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Home from "./pages/Home";
+import Refer from "./pages/Refer";
 
-class cApp extends Component{
-  
-  
-}
 
 function App() {
+  console.log(window.location.pathname)
   return (
+    
     <BrowserRouter>
       <div >
+      <NavBar/>
         <Routes>
-          
-          <Route path="/" element={ <Login/> } />
-          <Route path="/Login/*" element={ <AFoutput/> } />
-          
+        
+          <Route path="/" element={ <Home/> } />
+          <Route path="/proteinInput/*" element={ <AFoutput/> } />
+          <Route path="/Search/*" element={  <Search/> } />
+          <Route path="/About/*" element={  <About/> } />
+          <Route path="/Refer/*" element={  <Refer/> } />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function About() {
+  //return <div className="WordHome">Home</div>;
+  return (
+    <div className="HomeImg">
+      <img className="ProteinImg" src="/img/proteinImg.png" />
+    </div>
+  );
+}
