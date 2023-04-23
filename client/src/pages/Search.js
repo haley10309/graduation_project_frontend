e8eb08b87337890358f3aedd3a7034ec17e5cf72
hayung <yoohayung10309@gmail.com>
2023-04-24 02:10:59 +0900

    header server 4th

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -5,39 +5,44 @@ import { useEffect } from "react";
 import { useState } from "react";
 import instance from "../Request";
 
+
+
 export default function Search(){
     
-    const [protein, setProtein] = useState(""); //입력 값 변수 [입력값, 입력값 변경]
-
+  const [protein, setProtein] = useState(""); //입력 값 변수 [입력값, 입력값 변경]
+  const [proteinName, setProteinName] = useState(""); // 저장 후 변수
   const handleInput = (event) => {
     setProtein(event.target.value); //변수 저장 완료
     
   };
-
+  
   
    const confirm = async (event) => {
     // 확인 후 다음 페이지
     event.preventDefault();
+    setProteinName(protein)
     window.location.href = "/proteinInput";
-    await axios.post(
-      "http://localhost:3000/proteinInput",
-      {
-        protein
-      },
+    await axios.post("http://localhost:5000/api/Input",
+      {Name : proteinName},
       {
         headers : {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
       }
-    )
+    ).then(function (response) {
+      console.log(proteinName);
+     }).catch(function (error){
+      console.log(error);
+     })
+     
     
-    console.log(protein);
+    console.log(proteinName);
 
     
     //api post
       
-    localStorage.setItem("proteinName", protein);
+    localStorage.setItem("proteinName", proteinName);
     //localstorage 업로드
   };
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
-app.get('/api/proteinInput',(req,res) => {
+app.get('/api/Input',(req,res) => {
     res.send({message: 'Hello Express!!'});
 });
 
