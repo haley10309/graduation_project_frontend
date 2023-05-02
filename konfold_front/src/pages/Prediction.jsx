df0d24656fd20782b78321615b29e3c4f5d1dd7c
hayung <yoohayung10309@gmail.com>
2023-05-02 19:04:17 +0900

    di

--- konfold_front/src/pages/Prediction.jsx
+++ konfold_front/src/pages/Prediction.jsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
 export default function Prediction() {
+  const Wrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: #ccadbb78;
+  max-width: 62%;
+  margin: 0 auto;
+`;
   return (
     <div className="prediction-page">
       <h1 className="refer-title">
@@ -20,6 +28,18 @@ export default function Prediction() {
       <h1 className="korean-protein-expaination">
       단백질은 아미노산으로 구성된 생체 고분자로서, 펩타이드 결합에 의해 길게 연결된 폴리펩타이드(polypeptide) 사슬을 말한다.
       </h1>
+      
+      
+      <Wrap>
+      <div>
+        <img className="peptide_img" alt="role" src="/img/peptide_bond.jpeg" />
+      </div>
+      <div>
+        <img className="amino_img" alt="role" src="/img/amino_acid_residue.png" />
+      </div>
+      </Wrap>
     </div>
+    
+    
   );
 }
