157a3d8c8e0c58e695113416932d9af1619f5423
hayung <yoohayung10309@gmail.com>
2023-05-12 23:57:06 +0900

    reference pred_skills commit

--- konfold_front/src/pages/Pred_mech.jsx
+++ konfold_front/src/pages/Pred_mech.jsx
@@ -27,8 +27,8 @@ export default function Pred_mech(){
             단백질 구조 예측 AI
             </h1>
             <h1 className="title-N1">
-        2. 단백질 구조 예측 AI 메커니즘
-      </h1>
+            2. Alpha Fold
+            </h1>
       <h1 className="korean-protein-expaination">
       <Span /><Span />
       ‘Deepmind’에서 개발한 단백질 구조 예측 인공지능(AI) 프로그램
@@ -116,7 +116,7 @@ AlphaFold는 단백질 구조예측 학술대회 CASP(Critical Assessment of Str
         <Span /><Span />	
         -Amino acid paring 🡪 2차원 형태로 변환 🡪 pair representation (공간적 특징 반영)        </hi>
         <h1 className="korean-protein-expaination">
-        2단계. Evoformer, Attention 학습을 통해 전처리된 데이터에서 필요한 정보를 뽑아냄
+        2단계. Evoformer, Attention 학습을 통해 전처리된 데이터에서 필요한 정보 추출
        </h1>
        <div>
         <img className="process_img" alt="role" src="/img/evoformer_block.png" />
@@ -127,9 +127,58 @@ AlphaFold는 단백질 구조예측 학술대회 CASP(Critical Assessment of Str
       <h1 className="korean-protein-expaination">
       주어진 MSA representation과 pair representation을 self-attention 메커니즘에 적용하여 필요한 정보만 갖도록 개선하는 단계이다. 이 단계에서는 각각의 representation이 입력되어 단백질의 공간적, 진화적 관계에 대한 직접적인 추론을 가능하게 하는 정보를 교환함으로써 반복적으로 개선된다. 그 결과 단백질 구조에 대한 2D representation을 얻게 된다. 
        </h1>
+       <hi className="ref_left">
+        <Span /><Span />	
+        input: the initial version of the MSA representation and pair representation        
+        </hi>
+        <hi className="ref_left">
+        <Span /><Span />	
+        output: the final (best version) of the MSA representation and pair representation        
+        </hi>
+        <h1 className="korean-protein-expaination">
+        3단계. Structure module, 2단계에서 뽑아낸 정보를 구체적인 3차원 좌표로 처리
+         </h1>
+         <h1 className="korean-protein-expaination">
+         전 단계에서 만들어진 최종 2D representation 정보를 3D 좌표로 변환하는 단계이다. 이것은 weight를 공유하는 8개의 RNN 블록에서 수행된다. 초기 3D 좌표는 단백질의 모든 잔기가 동일한 위치와 방향을 갖게 배치된다. 그리고 RNN 블록 내에서 IPA(Invariant Point Attention) 연산을 사용한 업데이트, equivariant 업데이트, 비틀림 각도를 예측하는 과정을 거쳐 최종적인 3차원 좌표를 얻게 된다. 이때 출력단계에서 예측 위치의 정확도를 계산하는 과정도 포함된다.
+         </h1>
+         <hi className="ref_left">
+        <Span /><Span />	
+        input: final of MSA representation, Pair representation (2D representation)        
+        </hi>
+        <hi className="ref_left">
+        <Span /><Span />	
+        output: rotation, translation, angle
+         </hi>
+         <hi className="ref_left">
+        <Span /><Span />	
+        - Backbone (global frame) 예측
+         </hi>
+         <hi className="ref_left">
+        <Span /><Span />	
+        - Side chain (local frame) 예측
+         </hi>
+         <hi className="ref_left">
+        <Span /><Span />	
+        - pLDDT(per-residue confidence score, 잔류물별 신뢰도 메트릭): 예측의 신뢰도 자체 판단
+         </hi>
+         <h1 className="title-N1">
+         RoseTTA(Three-Track Attention)Fold
+      </h1>
+      <h1 className="korean-protein-expaination">
+      ‘Baker 연구팀’에서 AlphaFold에서 영감을 얻어 개발한 단백질 구조 예측 인공지능(AI) 프로그램
+AlphaFold2의 오픈소스가 공개되기 전 Baker 연구팀은 이와 비슷한 AI 모델을 개발했으며 AlphaFold2와 거의 비슷한 성능을 나타내 이목을 끌었다.
+         </h1>
+         <div>
+        <img className="process_img" alt="role" src="/img/perform_Roseta.png" />
+      </div>
+      <h1 className="korean-protein-expaination">
+      RoseTTAFold 또한 2022년 7월 논문과 오픈소스를 공개했으며 이에 따른 메커니즘은 다음과 같다.
+      </h1>
+      <div>
+        <img className="process_img" alt="role" src="/img/roseta_mech.png" />
+      </div>
 
 
-      
         </div>
     )
 }
--- konfold_front/src/pages/Pred_skills.jsx
+++ konfold_front/src/pages/Pred_skills.jsx
@@ -25,6 +25,22 @@ return (
         <h1 className="refer-title">
         단백질 구조 예측 AI
         </h1>
+        <h1 className="title-N1">
+        3. 주요 기술
+      </h1>
+      <h1 className="title-N1">
+      CNN(합성곱 신경망, Convolutional Neural Networks, ConvNet)
+      </h1>
+
+      <h1 className="korean-protein-expaination">
+      <Span /><Span />
+      Image를 분류하기 위해 1989년 Yann LeCun의 논문에서 고안된 알고리즘으로 Fully Connected Layer만으로 구성된 신경망의 한계를 보완한다. Image는 수많은 픽셀로 이루어져 있는 Matrix이며 픽셀은 0~255 사이의 숫자로 표현된다. 이러한 matrix를 일자로 쭉 펼쳐 실수로 구성된 vector를 input으로 하여 딥러닝 모델을 만들 수 있다.
+       </h1>
+
+
+
+
+
     </div>
 )
 }
