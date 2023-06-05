a2f372de3e51c6bc98d04859dc59a1a0d4589766
hayung <yoohayung10309@gmail.com>
2023-06-05 09:54:17 +0900

    search commit

--- konfold_front/src/AFoutput.jsx
+++ konfold_front/src/AFoutput.jsx
@@ -1,65 +1,60 @@
-import React, { useState } from "react";
+import React, { useState , Component} from "react";
 import { useEffect } from "react";
 import * as $3Dmol from '3dmol/build/3Dmol.js';
 import $ from 'jquery';
+import Search from "./pages/Search";
 
-export default function AFoutput() {
 
-  const [Id, setId] = useState([]);
-
-  
-
+export default function AFoutput()  {
+  const [Protein_input , setProtein_input] = useState("");
+  const script = document.createElement("script");
+  script.src = "https://3Dmol.org/build/3Dmol-min.js";
+  script.async = true;
+  document.body.appendChild(script);
   useEffect(() => {
-    fetch("/api/Input")
-        .then(res=> res.json())
-        .then(res => {
-          console.log(res);
-          setId(res);
-        }).catch(e => {
-          console.log("e", e)
-        })
-
-        // fetch("/api/Input", {
-        //   method : "POST",
-        //   headers : {
-        //     "Content-Type" : "application/json; charset=utf-8"
-        //   },
-        //   body : JSON.stringify(Id)
-        // })
-        // .then(res=> {
-        //   console.log(res);
-        //   //json 파싱전 , 상태 코드 확인 해서 res 값 초기화
-        // })
-        // .then(res=> {
-        //   console.log(res);
-        //   //res 값에 따른 결과 처리
-        //   if(res == null){
-        //     alert("등록 실패");
-        //   }
-        // })
-          
-
-
+    // fetch("/api/Input")
+    //     .then(res=> res.json())
+    //     .then(res => {
+    //       console.log(res);
+    //       setId(res);
+    //     });
+ 
     const script = document.createElement("script");
     script.src = "https://3Dmol.org/build/3Dmol-min.js";
     script.async = true;
     document.body.appendChild(script);
+    // setProtein_input(localStorage.getItem('proteinName'));
+    // console.log(Protein_input);
   },[]);
+
 
   // 시각화
   $(function() {
-    let element = $('#container-01');
+    let element = $('#id');
     let config = { backgroundColor: 'orange' };
     let viewer = $3Dmol.createViewer( element, config );
-    viewer.addSphere({ center: {x:0, y:0, z:0}, radius: 10.0, color: 'green' });
-    viewer.zoomTo();
-    viewer.render();
-    viewer.zoom(0.8, 2000);
+    let getId = "pdb:" + "1A00";
+
+    $3Dmol.download(getId, viewer, {}, function() {
+      viewer.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
+      viewer.zoomTo();
+      viewer.render();
+      viewer.zoom(0.8, 2000);
+  })
+
+
+    
   });
       
 
   return (
-  <div id="container-01" className="mol-container"></div>
+    <div className="page">
+      
+  <div id="id" className="mol-container"></div>
+  <li className="korean-protein-expaination"></li>
+  </div>
+
   );
+
 
   }
